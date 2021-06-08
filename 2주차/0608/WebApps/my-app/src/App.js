3e6e3043ed33fa5d6744df8b4093774ff2a38898
Namgon <zxcv9455@naver.com>
2021-06-08 11:38:48 +0900

    20210608[오전강의/리액트]
    20210608[오전강의/리액트]

--- 2주차/0608/WebApps/my-app/src/App.js
+++ 2주차/0608/WebApps/my-app/src/App.js
@@ -1,19 +1,54 @@
 import './App.css';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { Component } from 'react';
+
+// 기본 형태는 알고 있어야 한다.
+class Subject extends Component {
+  render() {
+    return(
+      <header>
+          <h1>React App</h1>
+      </header>
+    );
+  }
+}
+
+class Navigation extends Component {
+  render() {
+    return(
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Article extends Component {
+  render() {
+    return(
+      <article>
+        <h2>HTML?</h2>
+        <p>HTML이 가장 시웠어요. 리액트 써보시면 좋아요</p>
+      </article>
+    );
+  }
+}
 
 function App() {
+
+  // 화면에 출력되는 부분.
   return (
     <div>
-      <img src={logo} alt="logo.svg" />
-      <ul>
-        {/* 데이터 형식으로 작성한다고 생각하자 */}
-        <li style={{fontSize:"50px", color:"red"}}>HTML</li>
-        <li style={{fontSize:"50px"}}>CSS3</li>
-        <li style={{fontSize:"50px"}}>JavaScript</li>
-        
-      </ul>
+     {/* 컴포넌트는 다음과 같이 가져와야 한다. */}
+      <Subject></Subject>
+      <Navigation></Navigation>
+      <Article></Article>
     </div>
   );
 }
-
+// App.js를 다른곳에서 import할 수 있도록 export했다.(모듈화)
 export default App;
