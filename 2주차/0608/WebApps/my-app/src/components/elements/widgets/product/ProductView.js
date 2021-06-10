eb36e2868088a955e86cd22a9b7b0e748a33c7ee
Namgon <zxcv9455@naver.com>
2021-06-10 15:39:59 +0900

    20210610[쇼핑몰 리액트]
    20210610[쇼핑몰 리액트]
    - 구조 만들기

--- 2주차/0608/WebApps/my-app/src/components/elements/widgets/product/ProductView.js
+++ 2주차/0608/WebApps/my-app/src/components/elements/widgets/product/ProductView.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import productData from '../../../../db/products.json';
+import Rating from './Rating';
 
 export default function ProductView() {
 
+    const productList = productData.map(item => (
+
+            <div key={item.id} className="col-12 col-md-3 mb-4">
+                <div className="productImg"></div>
+                <p className="productTitle">{item.name}</p>
+                <p className="productScore">
+                    {/* a 이면서 b일떄 ? true : false */}
+                    {(item.rating && item.rating > 0) ? 
+                        (
+                            <Rating ratingValue={item.rating} />
+                        ) : ("")
+                    }
+                </p>
+                <p className="productPrice">${(item.price * (1 - item.discount/100)).toFixed(2)}<span id="pr">${item.price}</span> <span id="dis">{item.discount}%</span></p>
+            </div>
+
+    )).slice(0,20);
+
     return (
         <div className="row mt-5">
-            {/* <!-- product item--> */}
-            <div className="col-12 col-md-3 mb-4">
-                <div className="productImg"></div>
-                <p className="productTitle">Lorem ipsum jacket</p>
-                <p className="productScore"><i className="far fa-star"></i><i className="far fa-star"></i><i className="far fa-star"></i><i className="far fa-star"></i><i className="far fa-star"></i></p>
-                <p className="productPrice">$10.47<span>$17.45</span></p>
-            </div>
+            {productList}
         </div>
     );
 }
