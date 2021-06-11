ca73f6f26c8b31af43e314c9363a7992ec56ee02
Namgon <zxcv9455@naver.com>
2021-06-11 14:15:55 +0900

    20210611[오후]
    20210611[오후]
    - 블로그 추가

--- 2주차/0608/WebApps/my-app/src/components/elements/widgets/blog/BlogView.js
+++ 2주차/0608/WebApps/my-app/src/components/elements/widgets/blog/BlogView.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import blogData from '../../../../db/blog.json';
 
 export default function BlogView() {
+
+    const [newData, setNewData] = useState(blogData);
+
+    const blogDataList = newData.map(item => (
+        // {/* <!-- blog item--> */}
+        <div className="col-12 col-md-4 mb-4" key={item.id}>
+            <Link to={`${item.url}/${item.id}`}>
+                <div className="blogImg">
+                    <img src={item.image} alt=""/>
+                </div>
+            </Link>
+            
+                <div className="blogTxt">
+                    <p className="blogTitle">{item.title}</p>
+                    <Link to={`${item.authorUrl}/${item.id}`}>
+                        <p className="blogAuth">by {item.author}</p>
+                    </Link>
+                </div>
+            
+        </div>
+        // {/* <!-- item end --> */}
+    ));
+
     return (
         <div className="row mt-5">
-            {/* <!-- blog item--> */}
-            <div className="col-12 col-md-4 mb-4">
-                <div className="blogImg"></div>
-                <div className="blogTxt">
-                    <p className="blogTitle">A guide to latest trends</p>
-                    <p className="blogAuth">by admin</p>
-                </div>
-            </div>
-            {/* <!-- item end --> */}
+            {blogDataList}
         </div>
     );
 }
--- 2주차/0608/WebApps/my-app/src/components/elements/widgets/brand/Brand.js
+++ 2주차/0608/WebApps/my-app/src/components/elements/widgets/brand/Brand.js
@@ -1,53 +1,32 @@
-
+import { useState } from 'react';
+import BrandData from '../../../../db/nav.json';
 
 export default function Brand() {
+
+    const [brandData, setBrandData] = useState(BrandData);
+
+    const brandDataList = brandData.brand.map(item => (
+            // {/* <!-- 요소 --> */}
+            <div className="col-12 col-md-3" key={item.id}>
+                <div className="row">
+                    <div className="col-12 col-sm-4 brandImg"><i className={item.img}></i></div>
+                    <div className="col-12 col-sm-auto">
+                        <p className="brandTitle">{item.name}</p>
+                        <p className="brandTxt">{item.content}</p>
+                    </div>
+                </div>
+            </div>
+            // {/* <!-- 요소 끝--> */}
+    )).slice(0,4);
+
     return (
         <>
         {/* <!-- 콘텐츠 --> */}
         <section id="brand">
             <div className="container">
                 <div className="row">
-                    {/* <!-- 요소 --> */}
-                    <div className="col-12 col-md-3">
-                        <div className="row">
-                            <div className="col-12 col-sm-4 brandImg"><i className="fas fa-truck"></i></div>
-                            <div className="col-12 col-sm-auto">
-                                <p className="brandTitle">Free Shipping</p>
-                                <p className="brandTxt">Free shipping on all order</p>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <!-- 요소 끝--> */}
 
-                    <div className="col-12 col-md-3">
-                        <div className="row">
-                            <div className="col-12 col-sm-4 brandImg"><i className="fas fa-truck"></i></div>
-                            <div className="col-12 col-sm-auto">
-                                <p className="brandTitle">Free Shipping</p>
-                                <p className="brandTxt">Free shipping on all order</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-12 col-md-3">
-                        <div className="row">
-                            <div className="col-12 col-sm-4 brandImg"><i className="fas fa-truck"></i></div>
-                            <div className="col-12 col-sm-auto">
-                                <p className="brandTitle">Free Shipping</p>
-                                <p className="brandTxt">Free shipping on all order</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-12 col-md-3">
-                        <div className="row">
-                            <div className="col-12 col-sm-4 brandImg"><i className="fas fa-truck"></i></div>
-                            <div className="col-12 col-sm-auto">
-                                <p className="brandTitle">Free Shipping</p>
-                                <p className="brandTxt">Free shipping on all order</p>
-                            </div>
-                        </div>
-                    </div>
+                    {brandDataList}                   
                    
                 </div> 
                 {/* <!-- row end --> */}
